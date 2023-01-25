5f35ece4b68de6a2af8d3eff8d2b0f13f5323984
Ivan Havinson <havinsonivan@gmail.com>
2023-01-26 01:58:25 +0300

    added one more variant of brain-calc

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -12,9 +12,7 @@ def start_calc():
         num_1 = randint(1, 100)
         num_2 = randint(1, 100)
         operators = ['+', '-', '*']
-        for current_operator in operators:
-            current_operator = operators[randint(0, 2)]
-            break
+        current_operator = operators[randint(0, 2)]
         if current_operator == '+':
             answer = operator.add(num_1, num_2)
             question = f'Question: {num_1} + {num_2}'
@@ -25,5 +23,30 @@ def start_calc():
             question = f'Question: {num_1} * {num_2}'
             answer = operator.mul(num_1, num_2)
         answers_and_questions.append([answer, question])
+        i += 1
+    return answers_and_questions
+
+# code climate ругался на чрезмерную сложность.
+# этот вариант проще, но лишние вычисления.
+
+
+def start_calc_2():
+    answers_and_questions = []
+    i = 0
+    while i < 3:
+        num_1 = randint(1, 100)
+        num_2 = randint(1, 100)
+        addition_result = operator.add(num_1, num_2)
+        subtraction_result = operator.sub(num_1, num_2)
+        multiplication_result = operator.mul(num_1, num_2)
+        addition_question = f'Question: {num_1} + {num_2}'
+        subtraction_question = f'Question: {num_1} - {num_2}'
+        multiplication_question = f'Question: {num_1} * {num_2}'
+        results_and_questions_list = [
+            [addition_result, addition_question],
+            [subtraction_result, subtraction_question],
+            [multiplication_result, multiplication_question]]
+        answers_and_questions.append(
+            results_and_questions_list[randint(0, 2)])
         i += 1
     return answers_and_questions
