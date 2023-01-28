9e14ddba1a0e06dd91f62fe5dfea9d56ddf49dd4
Ivan Havinson <havinsonivan@gmail.com>
2023-01-28 21:22:32 +0300

    refactored is_prime in games/start_prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,18 +5,19 @@ rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(num):
-    for element in range(2, int(num ** 1 / 2)):
+    for element in range(2, int(num ** 1 / 2) + 1):
         if num % element == 0:
             return False
+    return True
 
 
 def start_prime():
     answers_and_questions = []
     num = randint(2, 100)
     question = f'Question: {num}'
-    answer = 'yes'
-    if is_prime(num) is False:
+    if is_prime(num) is True:
+        answer = 'yes'
+    else:
         answer = 'no'
-    print(answer)
     answers_and_questions.append([answer, question])
     return answers_and_questions
